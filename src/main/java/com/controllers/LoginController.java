9f8e2d4112b5fd738c6610fd5633a8ff789ab357
bejiVIII <simone0443@outlook.com>
2024-04-16 17:28:42 +0300

    login form process user input, add styling

--- src/main/java/com/controllers/LoginController.java
+++ src/main/java/com/controllers/LoginController.java
@@ -3,6 +3,7 @@ import com.models.LoginModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +14,11 @@ public class LoginController {
   public String displayLoginForm(Model model) {
     model.addAttribute("loginModel", new LoginModel());
     return "loginForm.html";
+  }
+
+  @PostMapping("/processLogin")
+  public String processLogin(LoginModel loginModel, Model model) {
+    model.addAttribute("loginModel", loginModel);
+    return "loginResults.html";
   }
 }
